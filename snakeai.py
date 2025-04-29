a2c84498df43b54ae2e5ab8eabdccbe4d516d6bf
TaigaTi <tarikabirch@gmail.com>
2025-04-28 20:01:13 -0400

    Created basic pygame loop

---
+++ snakeai.py
@@ -0,0 +1,63 @@
+import pygame
+
+pygame.init()
+
+# Screen Setup 
+screen_width = 1280
+screen_height = 720
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("SnakeAI")
+clock = pygame.time.Clock()
+
+running = True
+
+# Font Setup
+score_font = pygame.font.SysFont(None, 25)
+title_font = pygame.font.SysFont(None, 40)
+
+title_surface = title_font.render("Battle of the Algorithms", True, "white")
+
+# Player 1 Text 
+player1_name_surface = score_font.render("Player 1: Name", True, "white")
+player1_score_surface = score_font.render("Score: 0", True, "white")
+
+# Player 2 Text
+player2_name_surface = score_font.render("Player 2: Name", True, "white")
+player2_score_surface = score_font.render("Score: 0", True, "white")
+
+# Game Board Setup
+board_width = screen_width - 300
+board_height = screen_height - 200
+board = pygame.Surface((board_width, board_height))
+
+while running:
+    # Poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    # Change Background Color        
+    screen.fill("purple")
+    
+    # Draw Player and Score Info
+    screen.blit(title_surface, (screen_width/2 - 170, 50))
+    
+    screen.blit(player1_name_surface, (150, 100))
+    screen.blit(player1_score_surface, (150, 125))
+    
+    screen.blit(player2_name_surface, (screen_width - 270, 100))
+    screen.blit(player2_score_surface, (screen_width - 270, 125))
+    
+    # Draw Game Board
+    board.fill("blue")
+    
+    screen.blit(board, (150, 170))
+
+    # Render Display
+    pygame.display.flip()
+    
+    # Set Frame Rate
+    clock.tick(60)
+    
+pygame.quit()
