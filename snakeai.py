990824d54668cc6d4bae22347980f4875aa49589
TaigaTi <tarikabirch@gmail.com>
2025-05-07 08:55:42 -0400

    Added board design

--- snakeai.py
+++ snakeai.py
@@ -25,11 +25,14 @@ player1_score_surface = score_font.render("Score: 0", True, "white")
 # Player 2 Text
 player2_name_surface = score_font.render("Player 2: Name", True, "white")
 player2_score_surface = score_font.render("Score: 0", True, "white")
-
+ 
 # Game Board Setup
 board_width = screen_width - 300
 board_height = screen_height - 200
 board = pygame.Surface((board_width, board_height))
+border = pygame.Surface((board_width + 10, board_height + 10))
+board_background = pygame.image.load("assets/board.png")
+board_background = pygame.transform.scale(board_background, (board_width, board_height))
 
 while running:
     # Poll for events
@@ -37,8 +40,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
-    # Change Background Color        
-    screen.fill("purple")
+    # Set Background Color and Board Image  
+    screen.fill((11, 11, 11))
+
     
     # Draw Player and Score Info
     screen.blit(title_surface, (screen_width/2 - 170, 50))
@@ -49,10 +53,12 @@ while running:
     screen.blit(player2_name_surface, (screen_width - 270, 100))
     screen.blit(player2_score_surface, (screen_width - 270, 125))
     
-    # Draw Game Board
-    board.fill("blue")
-    
+    border.fill((63, 63, 63))
+    screen.blit(border, (145, 165))
     screen.blit(board, (150, 170))
+    
+    
+    board.blit(board_background, (0, 0))
 
     # Render Display
     pygame.display.flip()
