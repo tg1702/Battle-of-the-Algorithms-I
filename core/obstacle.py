a1863ac38098dcf989806bba431bccffce55987c
TaigaTi <tarikabirch@gmail.com>
2025-05-28 19:36:52 -0400

    Actually fixed obstacle logic fr fr

--- core/obstacle.py
+++ core/obstacle.py
@@ -20,9 +20,9 @@ class Obstacle:
             self.width = self.size
             self.height = self.size * self.length
 
-        max_x = (config.BOARD_HEIGHT - self.height) // self.size
-        max_y = (config.BOARD_WIDTH - self.width) // self.size
-
+        max_x = (config.BOARD_WIDTH - self.width) // self.size
+        max_y = (config.BOARD_HEIGHT - self.height) // self.size
+        
         self.x = random.randint(0, max_x) * self.size
         self.y = random.randint(0, max_y) * self.size
 
@@ -42,7 +42,7 @@ class Obstacle:
             else:
                 tile_x = self.x
                 tile_y = self.y + i * self.size
-
+                
             board.board.blit(self.tile_image, (tile_x, tile_y))
 
     def get_occupied_positions(self):
@@ -50,12 +50,14 @@ class Obstacle:
         Returns all grid positions occupied by this obstacle.
         """
         positions = []
+        
         for i in range(self.length):
             if self.orientation == "horizontal":
-                grid_x = (self.x + i * self.size) // self.size
-                grid_y = self.y // self.size
-            else:
                 grid_x = self.x // self.size
                 grid_y = (self.y + i * self.size) // self.size
+            else:
+                grid_x = (self.x + i * self.size) // self.size
+                grid_y = self.y // self.size
             positions.append((grid_x, grid_y))
+            
         return positions
